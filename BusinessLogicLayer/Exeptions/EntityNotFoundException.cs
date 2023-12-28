15b672f231b0e32a52993727d6975facc31df27d
NazarPetrakov <nazarpetrakov1@gmail.com>
2023-12-28 12:30:56 +0200

    add Exeptions

---
+++ BusinessLogicLayer/Exeptions/EntityNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿namespace BusinessLogicLayer.Exeptions
+{
+    internal class EntityNotFoundException<T> : Exception
+    {
+        public EntityNotFoundException() { }
+        public EntityNotFoundException(string message) : base(message) { }
+        public EntityNotFoundException(string message, Exception innerException) : base(message, innerException)
+        { }
+
+        public EntityNotFoundException(object key)
+            : base($"Entity \"{typeof(T).Name}\" ({key}) was not found.")
+        {
+        }
+    }
+}
